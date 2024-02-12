559034b9e9bbac51947d7e33584b288cd0a91d51
francinjr <francinaldomanoel135@gmail.com>
2024-02-12 12:40:51 -0300

    Add vote-on-poll, get-poll, cookies and validations

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,25 +1,22 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { z } from 'zod'
 import { PrismaClient } from '@prisma/client'
+import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
+import { voteOnPoll } from './routes/vote-on-poll'
+
 
 const app = fastify()
 
-const prisma = new PrismaClient()
-
-app.post('/polls', async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string()
-    })
-
-    const { title } = createPollBody.parse(request.body)
-
-    const poll = await prisma.poll.create({
-        data: {
-            title,
-        }
-    })
-    return reply.status(201).send({ pollId: poll.id })
+app.register(cookie, {
+    secret: "francinjr",
+    hook: 'onRequest',
 })
+
+app.register(createPoll)
+app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server runnig')
